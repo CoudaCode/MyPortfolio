052698aae6c4b5fd47638df7dccdc046d90e70c7
CoudaCode <couda.dm@gmail.com>
2023-08-20 11:33:25 +0000

    ok

--- client/src/utils/env.js
+++ client/src/utils/env.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 export const inProduction = process.env.NODE_ENV === 'production';
 
-export const apiUrl = inProduction ? 'https://my-portfolio-api-theta.vercel.app' : 'http://localhost:3000';
+export const apiUrl = inProduction ? '' : 'http://localhost:3000';
 
 
